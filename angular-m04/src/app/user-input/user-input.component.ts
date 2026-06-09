c01ee3baa5ed33837fdce53972e36f8262defcd0
costinchelu <costin.chelu@gmail.com>
2026-06-09 12:57:21 +0300

    .

--- angular-m04/src/app/user-input/user-input.component.ts
+++ angular-m04/src/app/user-input/user-input.component.ts
@@ -1,5 +1,6 @@
-import {Component} from "@angular/core";
+import {Component, EventEmitter, Output} from "@angular/core";
 import {FormsModule} from "@angular/forms";
+import type {InvestmentInput} from "../investment.model";
 
 @Component({
   selector: 'app-user-input',
@@ -10,12 +11,19 @@ import {FormsModule} from "@angular/forms";
 })
 export class UserInputComponent {
 
+  @Output() calculate = new EventEmitter<InvestmentInput>();
+
   twoWayInitialInvestment = '0';
   twoWayAnnualInvestment = '0';
   twoWayExpectedReturn = '0';
   twoWayDuration = '0';
 
   onSubmit() {
-
+    this.calculate.emit({
+      initialInvestment: +this.twoWayInitialInvestment,
+      annualInvestment: +this.twoWayAnnualInvestment,
+      expectedReturn: +this.twoWayExpectedReturn,
+      duration: +this.twoWayDuration,
+    });
   }
 }
